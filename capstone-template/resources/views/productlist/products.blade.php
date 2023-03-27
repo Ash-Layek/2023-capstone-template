a2c630e66d88795c9cb44935f993af550fd8692b
Ash-Layek <achraf.layek1@gmail.com>
2023-03-27 19:55:36 -0300

    fixed productlist listing for items and categories

--- capstone-template/resources/views/productlist/products.blade.php
+++ capstone-template/resources/views/productlist/products.blade.php
@@ -14,7 +14,7 @@ Laravel Project
 
 
 <div class="row">
-  <div class="col-md-4 col-md-offset-0 ">
+  <div class="col-md-8 col-md-offset-0 ">
     <table class="table">
       @foreach ($categories as $item)    
         <tr>
@@ -26,8 +26,24 @@ Laravel Project
   <div class="col-md-4 col-md-offset-0 ">
     <table class="table">
       @foreach ($items as $item)    
+	
         <tr>
-          <td><a href="">{{ $item->title }}</a></td>
+		
+          <td>{{ $item->title }}</td>
+		  <td>{{ $item->price }}</td>
+		  <td>
+            @if (Storage::disk('public')->exists('images/items/' . $item->picture))
+                <img src="{{ asset('storage/images/items/' . $item->picture) }}" alt="{{ $item->title }} " width="100" />
+            @else
+                No image available
+            @endif
+
+			<td><button> Buy now </button></td>
+        </td>
+		 
+		  
+		  
+		  
         </tr> 
       @endforeach
     </table>
